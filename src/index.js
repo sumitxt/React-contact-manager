8ee6bbb40953c79248b24250ab05a6c6d4186ab8
sumitxt <sumitbarua14@gmail.com>
2022-12-28 19:44:55 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(
-    <h1>Hello, React!</h1>,
-    document.getElementById('root')
-);
+// //ReactDom
+// ReactDOM.render(
+//     <h1>Hello, React!</h1>,
+//     document.getElementById('root')
+// );
+
+
+// //ReactDom Root
+// const name = "Sumit";
+// const el = <p>Hello, {name}</p>;
+
+// ReactDOM.render(
+//   el,
+//   document.getElementById('root')
+// ); 
+
+
+//Counter every 1 ms
+let counter = 0;
+
+function show() {
+  counter++;
+  const el = <p>{counter}</p>;
+  ReactDOM.render(
+    el, document.getElementById('root')
+  );
+}
+//setInterval calls show every 1 ms
+setInterval(show, 1); 
